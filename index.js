469b022b0cebdf73af9d1c1c6f4d83b321a6ea70
Muhammad Rafai <rafaimhd123@gmail.com>
2023-11-02 16:54:36 +0700

    make logic to upload to CloudFlare R2

--- index.js
+++ index.js
@@ -5,12 +5,15 @@
 const express = require("express")
 const multer = require("multer")
 const cors = require("cors")
+const {S3Client, PutObjectCommand} = require("@aws-sdk/client-s3")
+const dotenv = require("dotenv")
 
 const app = express()
 const port = 3000
 
 // middleware
 app.use(cors())
+dotenv.config()
 
 const storage = multer.memoryStorage()
 const upload = multer({
@@ -22,8 +25,26 @@ app.get("/", (req, res) => {
     res.send("Hello World!")
 })
 
-app.post("/upload", upload.single("file"), (req, res) => {
-    console.log(req.file)
+app.post("/upload", upload.single("file"), async (req, res) => {
+    // console.log(req.file)
+    // res.send("File Upload")
+    const S3 = new S3Client({
+        region: "auto",
+        endpoint: process.env.ENDPOINT,
+        credentials: {
+            accessKeyId: process.env.R2_ACCESS_KEY_ID,
+            secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
+        }
+    })
+
+    await S3.send(
+        new PutObjectCommand({
+            Body: req.file.buffer,
+            Bucket: "fullstack-team",
+            Key: req.file.originalname,
+            ContentType: req.file.mimetype
+        })
+    )
     res.send("File Upload")
 })
 
